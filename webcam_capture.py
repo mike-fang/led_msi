d5301ead2016e59f2a3e7d3469c1297424b78457
Michael Fang <1michaelfang@gmail.com>
2020-06-19 15:25:41 -0700

    init commit with cv2 webcam example

--- webcam_capture.py
+++ webcam_capture.py
@@ -4,6 +4,10 @@ from time import sleep
 import os
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
+imgs_dir = os.path.join(curr_dir, 'imgs')
+if not os.path.isdir(imgs_dir):
+    print(f'Making directory {imgs_dir}')
+    os.mkdir(imgs_dir)
 
 webcam = cv2.VideoCapture(0)
 for n in range(5):
