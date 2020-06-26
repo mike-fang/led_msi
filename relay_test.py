8eff149ba97706a9d3bb5026a1816245ac54eb84
Ansa Schmulbach <viansa02@gmail.com>
2020-06-26 14:14:17 -0700

    LED control with webcam

--- relay_test.py
+++ relay_test.py
@@ -2,6 +2,11 @@ from relay_ft245r import relay_ft245r
 import sys
 import time
 import numpy as np
+import cv2
+import os
+
+debug = False
+colors = ('z_blank', 'zz_blank', 'zzz_blank', 'yellow', 'white', 'blue', 'red', 'green')
 
 def get_intervals(periods, tmax):
     intervals = []
@@ -19,6 +24,19 @@ def get_diff(intervals):
         t0 = t
     return diff
 
+def test(rb):
+    rb.switchon(3)
+    time.sleep(1)
+    rb.switchoff(3)
+
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+imgs_dir = os.path.join(curr_dir, 'imgs')
+if not os.path.isdir(imgs_dir):
+    print(f'Making directory {imgs_dir}')
+    os.mkdir(imgs_dir)
+
+webcam = cv2.VideoCapture(0)
+
 if __name__ == '__main__':
     rb = relay_ft245r.FT245R()
     dev_list = rb.list_dev()
@@ -32,25 +50,42 @@ if __name__ == '__main__':
     print('Using device with serial number ' + str(dev.serial_number))
 
     rb.connect(dev)
-    periods = (0.2, 0.3, 0.5, 0.7, 1.1, 1.3, 1.7, 1.9)
-    _, diff = get_intervals(periods, 5)
 
-    for n, dt in diff:
-        time.sleep(dt)
-        rb.toggle(n+1)
+    if debug:
+        test(rb)
 
-    rb.set_state(np.zeros(8))
+    for i in range(1, 9):
+        rb.switchon(i)
+        time.sleep(.5)
+        ret, frame = webcam.read()
+        f_name = os.path.join(curr_dir, 'imgs', f'frame_{colors[i - 1]}.png')
+        print(f'Saving image to {f_name}')
+        cv2.imwrite(f_name, frame)
+        time.sleep(.5)
+        rb.switchoff(i)
 
-    for n in range(8):
-        time.sleep(.1)
-        state = np.zeros(8)
-        state[n] = 1
-        rb.set_state(state)
 
-    for n in range(64):
-        time.sleep(1/(n+1))
-        state = np.zeros(8)
-        state[n*3 % 8] = 1
-        rb.set_state(state)
+    #Example code
+    if debug:
+        periods = (0.2, 0.3, 0.5, 0.7, 1.1, 1.3, 1.7, 1.9)
+        _, diff = get_intervals(periods, 5)
 
-    rb.set_state(np.zeros(8))
+        for n, dt in diff:
+            time.sleep(dt)
+            rb.toggle(n+1)
+
+        rb.set_state(np.zeros(8))
+
+        for n in range(8):
+            time.sleep(.1)
+            state = np.zeros(8)
+            state[n] = 1
+            rb.set_state(state)
+
+        for n in range(64):
+            time.sleep(1/(n+1))
+            state = np.zeros(8)
+            state[n*3 % 8] = 1
+            rb.set_state(state)
+
+        rb.set_state(np.zeros(8))
